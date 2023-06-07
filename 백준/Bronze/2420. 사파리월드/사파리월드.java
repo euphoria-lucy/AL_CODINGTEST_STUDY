f8b19294e41506a621230b00c6dedd8c1262bc53
MISUNG <113699085+euphoria-lucy@users.noreply.github.com>
2023-06-07 20:15:58 +0900

    [Bronze V] Title: 사파리월드, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 백준_2420_사파리월드
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+
+        System.out.println(Math.abs(A - B));
+
+    }
+}
