b195f5162dd30dbee1af43d6cf257b99b70cfa7a
MISUNG <113699085+euphoria-lucy@users.noreply.github.com>
2023-06-07 20:27:18 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 196 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 백준_2743_단어 길이 재기
+
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+
+        System.out.println(word.length());
+
+    }
+}
