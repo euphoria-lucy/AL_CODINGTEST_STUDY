7024da2f1e0e838b62d90bd2e21bdc92f2ea0199
MISUNG <113699085+euphoria-lucy@users.noreply.github.com>
2023-06-07 20:26:23 +0900

    [Bronze V] Title: N 찍기, Time: 1116 ms, Memory: 32528 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 백준_2741_N찍기
+
+        Scanner sc = new Scanner(System.in);
+
+        int num_o = sc.nextInt();
+
+        for (int i = 1; i <= num_o; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
