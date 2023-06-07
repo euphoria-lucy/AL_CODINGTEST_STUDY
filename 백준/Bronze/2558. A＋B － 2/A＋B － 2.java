965ece788368bf187a1636dd8521ceaecd395809
MISUNG <113699085+euphoria-lucy@users.noreply.github.com>
2023-06-07 20:19:23 +0900

    [Bronze V] Title: A+B - 2, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 백준_2558_ A + B - 2
+
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+
+    }
+}
