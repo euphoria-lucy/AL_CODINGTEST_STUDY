9ca3d9afd8086501e35979e03e2c7917651babd7
MISUNG <113699085+euphoria-lucy@users.noreply.github.com>
2023-06-26 14:13:01 +0900

    [Bronze V] Title: 카드 게임, Time: 204 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int []a = new int[5];
+        int sum=0;
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i=0;i<5;i++){
+            a[i] = scanner.nextInt();
+            sum += a[i];
+        }
+        System.out.println(sum);
+        
+    }
+  }
