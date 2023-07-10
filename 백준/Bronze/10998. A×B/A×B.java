6f0b8eb9c23caee6587c03060fbd26f9a43f667f
MISUNG <113699085+euphoria-lucy@users.noreply.github.com>
2023-07-11 08:46:00 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num_o = sc.nextInt();
+        int num_t = sc.nextInt();
+
+        System.out.println(num_o * num_t);
+
+    }
+}
